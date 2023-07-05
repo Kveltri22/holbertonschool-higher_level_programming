a32b506524976231fe7bb439eb99919f02e9edea
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-07-05 16:03:37 -0500

    Update 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
--- This is not a comment
-SELECT * FROM mysql.user WHERE user = 'user_0d_1';
-SELECT * FROM mysql.user WHERE user = 'user_0d_2';
+-- Lists all priveleges of the users user_0d_1 and user_0d_2.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
