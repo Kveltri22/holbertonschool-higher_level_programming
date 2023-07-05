85073b919b314b807b88cef5e61c8e6b2306da2c
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-07-05 11:03:11 -0500

    Update 2-my_filter_states.py

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
-""" takes in an argument and displays all values in the states table """
-
+# Displays all values in the states table of the database hbtn_0e_0_usa
+# whose name matches that supplied as argument.
+# Usage: ./2-my_filter_states.py <mysql username> \
+#                                <mysql password> \
+#                                <database name> \
+#                                <state name searched>
+""" Module doc """
+import sys
 import MySQLdb
 
-
-def print_n_state():
-    from sys import argv
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], database=argv[3])
-
-    cur = db.cursor()
-
-    cur.execute("SELECT * FROM states WHERE name\
-        LIKE binary %s", [format(argv[4])])
-    for rows in cur.fetchall():
-        print(rows)
-
-    cur.close()
-    db.close()
-
-
 if __name__ == "__main__":
-    print_n_state()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
