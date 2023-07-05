3b9c0d743cbdab664778b133062c22f8d5689be5
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-07-05 10:54:09 -0500

    Update 12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-""" Changes the name of a State object """
+# Changes the name of the State object with id = 2 to
+# New Mexico in the database hbtn_0e_6_usa.
+# Usage: ./12-model_state_update_id_2.py <mysql username> /
+#                                        <mysql password> /
+#                                        <database name>
+""" MOD DOC """
 import sys
-from model_state import Base, State
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from sqlalchemy import (create_engine)
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    item = session.query(State).filter_by(id="2").first()
-    item.name = "New Mexico"
-
+    state = session.query(State).filter_by(id=2).first()
+    state.name = "New Mexico"
     session.commit()
-
-    session.close()
