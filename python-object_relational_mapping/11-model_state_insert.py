a19eedf7ce1b86ba256a756be4ffb7ed03330d55
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-07-05 10:52:56 -0500

    Update 11-model_state_insert.py

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-""" Prints the State object with the name passed as argument """
-
+# Adds the State object "Louisiana" to the database hbtn_0e_6_usa.
+# Usage: ./11-model_state_insert.py <mysql username> /
+#                                   <mysql password> /
+#                                   <database name>
+""" MOD DOC """
 import sys
-from model_state import Base, State
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from sqlalchemy import (create_engine)
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_state = State(name="Louisiana")
-    session.add(new_state)
-    item = session.query(State).filter_by(name="Louisiana").first()
-    print("{}".format(item.id))
-    session.commit()
 
-    session.close()
+    louisiana = State(name="Louisiana")
+    session.add(louisiana)
+    session.commit()
+    print(louisiana.id)
