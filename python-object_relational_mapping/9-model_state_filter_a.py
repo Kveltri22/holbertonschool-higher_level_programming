f96729c552da9b8bd6f5228749602bdbba49e6de
Kveltri22 <118138726+Kveltri22@users.noreply.github.com>
2023-07-05 11:07:34 -0500

    Update 9-model_state_filter_a.py

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-""" Start link class to table in database """
+# Lists all State objects that contain the letter a
+# from the database hbtn_0e_6_usa.
+# Usage: ./9-model_state_filter_a.py <mysql username> /
+#                                    <mysql password> /
+#                                    <database name>
+""" MOD DOC """
 import sys
-from model_state import Base, State
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
-from sqlalchemy import (create_engine)
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for item in session.query(State).filter(State.name.like
-                                            ('%a%')).order_by(State.id):
-        print("{}: {}".format(item.id, item.name))
 
-    session.close()
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
